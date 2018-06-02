6e0f3a431ac6075a1671e69d0004b46087d42b95
gushwell <gushwell@gmail.com>
2018-06-02 10:27:06 +0900

    コイン15パズル

---
+++ Coin15Puzzle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Coin15Puzzle {
+    class Program {
+        static void Main(string[] args) {
+            var solver = new Solver();
+            foreach (var x in solver.Solve()) {
+                x.Print();
+            }
+        }
+    }
+}
