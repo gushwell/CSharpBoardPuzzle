fe1b4ea65406d0641d15d3d8496d23beea979022
gushwell <gushwell@gmail.com>
2018-06-09 16:51:59 +0900

    N-Queen パズル

--- NQueenPuzzle/Program.cs
+++ NQueenPuzzle/Program.cs
@@ -4,7 +4,7 @@ namespace NQueenPuzzle {
     class Program {
         static void Main(string[] args) {
             var solver = new Solver();
-            var bs = solver.Solve(8);
+            var bs = solver.Solve(5);
             var count = 0;
             foreach (var b in bs) {
                 b.Print();
